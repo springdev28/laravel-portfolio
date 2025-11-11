f614f2fb1946386b69c8ad8429990d596297040a
Bahar Yüksel <baharyuksel0403@gmail.com>
2025-11-11 20:09:57 +0300

    Delete CourseApp directory

--- CourseApp/app9/partials/_menu.php
+++
@@ -1,8 +0,0 @@
-<div class="class list-group">
-    <?php for ($i = 0; $i < count($categories); $i++): ?>
-
-        <a href="" class="list-group-item list-group-item-action"><?php echo $categories[$i]; ?></a>
-
-    <?php endfor ?>                
-
-</div>
